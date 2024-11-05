423bac8e05a3eb95f7a8c2e690f8bd5b302f10ec
DivineDris <save200104@gmail.com>
2024-11-05 19:04:44 +0300

    intitial commit

---
+++ App/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppLogger.Logger.Log("Logger_is_running");
+        }
+    }
+}
---
+++ AppLogger/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Humanizer;
+
+namespace AppLogger
+{
+    public class Logger
+    {
+        public static void Log(string text)
+        {
+            Console.WriteLine("Not humanized text: "+ text);
+            Console.WriteLine("Humanized text: "+text.Humanize());
+        }
+    }
+}
